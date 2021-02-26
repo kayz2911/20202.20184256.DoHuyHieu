6288b9196d24be3ea785f5149b73bc350a84e449
Do Hieu <dohuyhieu2911@gmail.com>
2021-02-26 14:02:21 +0700

    first dialog

---
+++ FirstDialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class FirstDiaLog{
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, "Hello world!How are you??");
+		System.exit(0);
+	}
+}
