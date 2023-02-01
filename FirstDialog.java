89410818b81afcc9d650b343935ae232af5631e2
Do Hieu <76253031+kayz2911@users.noreply.github.com>
2023-02-01 14:43:26 +0700

    repo

--- FirstDialog.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class FirstDiaLog{
-	public static void main(String[] args){
-		JOptionPane.showMessageDialog(null, "Hello world!How are you??");
-		System.exit(0);
-	}
-}
--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld{
-	public static void main(string args[]){
-		System.out.println("Hello \t world");
-	} // end of method main
-}
